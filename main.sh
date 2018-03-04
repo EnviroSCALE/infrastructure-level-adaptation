d123dd70e4c5c7a21035921f32def6ce9ea29f9e
amatur <amatur003@gmail.com>
2018-03-04 19:52:33 +0600

    EnviroSCALE simulator commit 1

---
+++ main.sh
@@ -0,0 +1,9 @@
+# Serial of arguments
+
+#~ MM = sys.argv[1]  # time
+#~ DD = sys.argv[2]  # data budget
+#~ UU = sys.argv[3]  # upload interval
+#~ CC = sys.argv[4]  # choice 1 2 3
+#~ PP = sys.argv[5]  # period update # fix at 30
+
+python dispatcher.py 60 10000 10 3 > file10000_3.txt;
